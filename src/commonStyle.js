d93ff5f66a04479c16698bb67887ac6b1ef74730
Jhelison <jhelisong@gmail.com>
2021-05-12 09:59:52 -0300

    Started the initscreen design

--- src/commonStyle.js
+++ src/commonStyle.js
@@ -1,109 +1,123 @@
-import { Dimensions, Platform, PixelRatio } from 'react-native'
+import { Dimensions, Platform, PixelRatio } from "react-native"
 
-const {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-} = Dimensions.get('window');
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window")
 
 const scale = SCREEN_WIDTH / 320
 
 const normalize = (size) => {
     const newSize = (size * scale) / 1.4
-    if(Platform.OS === 'ios'){
+    if (Platform.OS === "ios") {
         return Math.round(PixelRatio.roundToNearestPixel(newSize))
-    }
-    else{
+    } else {
         return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
     }
 }
 
 export default {
-    fontFamily: 'OpenSans-Regular',
-    fontFamilyExtraBold: 'OpenSans-ExtraBold',
-    fontFamilyExtraBoldItalic: 'OpenSans-ExtraBoldItalic',
+    fontFamily: "OpenSans-Regular",
+    fontFamilyExtraBold: "OpenSans-ExtraBold",
+    fontFamilyExtraBoldItalic: "OpenSans-ExtraBoldItalic",
     colors: {
-        primary: 'white',
-        secondary: '#fc8621',
-        terciary: '#16697a',
-        background: '#f6f6f6', 
-        mainText: '#222',
-        subText: '#555',
-        separator: '#e6e6e6',
-        alertColors:{
-            faturado: '#339900',
-            enviado: '#99cc33',
-            espera: 'gray',
-            error: '#ED553B',
-            alert: '#6930c3'
-        }
+        primary: "white",
+        secondary: "#fc8621",
+        terciary: "#16697a",
+        background: "#f6f6f6",
+        mainText: "#222",
+        subText: "#555",
+        separator: "#e6e6e6",
+        alertColors: {
+            faturado: "#339900",
+            enviado: "#99cc33",
+            espera: "gray",
+            error: "#ED553B",
+            alert: "#6930c3",
+        },
     },
-    fontSize:{
+    fontSize: {
         pageTitle: normalize(20),
         bodyText: normalize(14),
         modalText: normalize(14),
-        listItem:{
+        listItem: {
             title: normalize(16),
             secondaryText: normalize(14),
-            biggerText: normalize(25)
+            biggerText: normalize(25),
         },
         menuTitle: normalize(14),
-        tab: normalize(14), 
+        tab: normalize(14),
         button: normalize(14),
         formText: normalize(16),
         errorText: normalize(12),
         finalTotal: normalize(30),
-        bigMessage: normalize(50)
+        bigMessage: normalize(50),
     },
-    spacers:{
-        margin:{
-            horizontal: normalize(10),
-            vertical: normalize(5)
-        },
-        padding:{
+    spacers: {
+        margin: {
             horizontal: normalize(10),
             vertical: normalize(5),
-            plusMinusButtons: normalize(15)
-        }
-    },
-    heighs:{
-        tabNavigator:{
-            headerHeight: normalize(30),
-            tabsHeight: normalize(45)
         },
-        NewBudget:{
+        padding: {
+            horizontal: normalize(10),
+            vertical: normalize(5),
+            plusMinusButtons: normalize(15),
+        },
+    },
+    heighs: {
+        tabNavigator: {
+            headerHeight: normalize(30),
+            tabsHeight: normalize(45),
+        },
+        NewBudget: {
             customContainer: normalize(50),
             totalContainer: normalize(80),
             buttons: normalize(100),
-            totalPriceButton: normalize(50)
+            totalPriceButton: normalize(50),
         },
-        BudgetProduct:{
+        BudgetProduct: {
             main: normalize(190),
             name: normalize(70),
             price: normalize(50),
             priceChangers: normalize(50),
-            customerButton: normalize(50)
+            customerButton: normalize(50),
         },
-        buttons:{
+        buttons: {
             height: normalize(50),
-            width: normalize(100)
+            width: normalize(100),
         },
         sphere: normalize(15),
-        home:{
-            noticeWindow: normalize(300)
-        }
+        home: {
+            noticeWindow: normalize(300),
+        },
     },
-    borderRadius:{
+    borderRadius: {
         main: normalize(10),
-        plusMinusButtons: normalize(25)
+        plusMinusButtons: normalize(25),
     },
-    iconSizes:{
+    iconSizes: {
         main: normalize(25),
         bigger: normalize(30),
-        giant: normalize(100)
+        giant: normalize(100),
     },
-    waveProperty:[
-            {A: normalize(20), T: normalize(300), fill: '#62c2ff'},
-            {A: normalize(30), T: normalize(250), fill: '#0087dc'},
-            {A: normalize(40), T: normalize(230), fill: '#1aa7ff'},
-    ]
+    waveProperty: [
+        { A: normalize(20), T: normalize(300), fill: "#62c2ff" },
+        { A: normalize(30), T: normalize(250), fill: "#0087dc" },
+        { A: normalize(40), T: normalize(230), fill: "#1aa7ff" },
+    ],
+    firstInitScreen: {
+        statusSphere: {
+            size: normalize(70),
+            boderWidth: normalize(5),
+            activeBorder: "#4a47a3",
+            inactiveBorder: "#d8e3e7",
+        },
+        divider: {
+            height: normalize(10),
+        },
+        bottomButtons: {
+            inactive: "#eeeeee",
+            inactiveBorder: "#d8e3e7",
+            done: "#4aa96c",
+            error: "#f55c47",
+            size: normalize(80),
+        },
+    },
 }
--- src/screens/FirstInitScreen.js
+++ src/screens/FirstInitScreen.js
@@ -1,23 +1,340 @@
-import React, { useState } from 'react'
+import React, { useState } from "react"
 import {
     View,
     Text,
-    StyleSheet
-} from 'react-native'
+    StyleSheet,
+    StatusBar,
+    TouchableOpacity,
+} from "react-native"
 
-import commonStyle from '../commonStyle'
+import commonStyle from "../commonStyle"
 
-const FirstInitScreen = props => {
+import Icon from "react-native-vector-icons/Feather"
+
+const FirstInitScreen = (props) => {
+    const [screenStep, setScreenStep] = useState(0)
+    const [haverError, setHaveError] = useState(false)
+
+    const getScreen = (step) => {
+        if (step === 0) {
+            return (
+                <View style={styles.step0Container}>
+                    <View
+                        style={[
+                            { alignSelf: "center", flex: 1 },
+                            styles.centerContent,
+                        ]}
+                    >
+                        <Text
+                            style={[
+                                styles.title,
+                                {
+                                    fontSize: 24,
+                                    color:
+                                        commonStyle.firstInitScreen.statusSphere
+                                            .activeBorder,
+                                },
+                            ]}
+                        >
+                            Seja Bem Vindo ao PDV!
+                        </Text>
+                        <Text style={styles.title}>
+                            Vamos te deixar pronto para realizar vendas logo
+                            após estes passos
+                        </Text>
+                        <Icon
+                            name="thumbs-up"
+                            size={60}
+                            color={
+                                commonStyle.firstInitScreen.statusSphere
+                                    .activeBorder
+                            }
+                        />
+                    </View>
+                </View>
+            )
+        } else if (step === 1) {
+            return (
+                <View>
+                    <Text>Stepp 1</Text>
+                </View>
+            )
+        } else if (step === 2) {
+            return (
+                <View>
+                    <Text>Stepp 2</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View>
+                    <Text>Stepp else</Text>
+                </View>
+            )
+        }
+    }
 
     return (
-        <View>
-            <Text>WOW!</Text>
+        <View style={styles.mainContainer}>
+            <StatusBar backgroundColor="white" barStyle="dark-content" />
+            <View style={styles.statusBar}>
+                {/* Sphere container index 1 */}
+                <View style={styles.sphereContainer}>
+                    <Text
+                        style={[
+                            styles.statusSphereLabel,
+                            styles.labelInactive,
+                            haverError && screenStep === 1
+                                ? styles.labelError
+                                : screenStep === 1
+                                ? styles.labelActive
+                                : screenStep > 1
+                                ? styles.labelDone
+                                : null,
+                        ]}
+                    >
+                        Servidor
+                    </Text>
+                    <View
+                        style={[
+                            styles.sphere,
+                            styles.sphereInactive,
+                            haverError && screenStep === 1
+                                ? styles.sphereError
+                                : screenStep === 1
+                                ? styles.sphereActive
+                                : screenStep > 1
+                                ? styles.sphereDone
+                                : null,
+                        ]}
+                    >
+                        {haverError && screenStep === 1 ? (
+                            <Icon
+                                name="x"
+                                size={30}
+                                color={
+                                    commonStyle.firstInitScreen.bottomButtons
+                                        .error
+                                }
+                            />
+                        ) : screenStep > 1 ? (
+                            <Icon
+                                name="check"
+                                size={30}
+                                color={
+                                    commonStyle.firstInitScreen.bottomButtons
+                                        .done
+                                }
+                            />
+                        ) : null}
+                    </View>
+                </View>
+                <View style={[styles.divider, screenStep === 2 ? styles.dividerDone : null]} />
+                {/* Sphere container index 2 */}
+                <View style={styles.sphereContainer}>
+                    <Text
+                        style={[
+                            styles.statusSphereLabel,
+                            styles.labelInactive,
+                            haverError && screenStep === 2
+                                ? styles.labelError
+                                : screenStep === 2
+                                ? styles.labelActive
+                                : screenStep > 2
+                                ? styles.labelDone
+                                : null,
+                        ]}
+                    >
+                        Usuário
+                    </Text>
+                    <View
+                        style={[
+                            styles.sphere,
+                            styles.sphereInactive,
+                            haverError && screenStep === 2
+                                ? styles.sphereError
+                                : screenStep === 2
+                                ? styles.sphereActive
+                                : screenStep > 2
+                                ? styles.sphereDone
+                                : null,
+                        ]}
+                    >
+                        {haverError && screenStep === 2 ? (
+                            <Icon
+                                name="x"
+                                size={30}
+                                color={
+                                    commonStyle.firstInitScreen.bottomButtons
+                                        .error
+                                }
+                            />
+                        ) : screenStep > 2 ? (
+                            <Icon
+                                name="check"
+                                size={30}
+                                color={
+                                    commonStyle.firstInitScreen.bottomButtons
+                                        .done
+                                }
+                            />
+                        ) : null}
+                    </View>
+                </View>
+            </View>
+            {getScreen(screenStep)}
+            <View style={styles.bottomButonsContainer}>
+                {/* Back button */}
+                <TouchableOpacity
+                    style={[styles.bottomButtons, screenStep != 0 ? styles.bottomButtonsActive : styles.bottomButtonsInactive]}
+                    onPress={() => setScreenStep((oldState) => oldState - 1)}
+                    disabled={screenStep === 0}
+                >
+                    <Icon
+                        name="chevron-left"
+                        size={50}
+                        color={
+                            commonStyle.firstInitScreen.bottomButtons
+                                .inactiveBorder
+                        }
+                    />
+                </TouchableOpacity>
+                {/* Forward button */}
+                <TouchableOpacity
+                    style={[
+                        styles.bottomButtons,
+                        screenStep != 2
+                            ? styles.bottomButtonsActive
+                            : styles.bottomButtonsDone,
+                        haverError ? styles.bottomButtonsError : null,
+                    ]}
+                    onPress={() => setScreenStep((oldState) => oldState + 1)}
+                    disabled={haverError}
+                >
+                    {haverError ? (
+                        <Icon name="x" size={50} color={"white"} />
+                    ) : screenStep != 2 ? (
+                        <Icon name="chevron-right" size={50} color={"white"} />
+                    ) : (
+                        <Icon name="check" size={50} color={"white"} />
+                    )}
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    mainContainer: {
+        backgroundColor: "white",
+        flex: 1,
+    },
+    step0Container: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+    },
+    centerContent: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        textAlign: "center",
+        fontFamily: commonStyle.fontFamilyExtraBold,
+        fontSize: commonStyle.fontSize.pageTitle,
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+    },
+    statusBar: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    sphereContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    sphere: {
+        width: commonStyle.firstInitScreen.statusSphere.size,
+        height: commonStyle.firstInitScreen.statusSphere.size,
+        borderRadius: commonStyle.firstInitScreen.statusSphere.size / 2,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: commonStyle.firstInitScreen.statusSphere.boderWidth,
+    },
+    sphereActive: {
+        borderColor: commonStyle.firstInitScreen.statusSphere.activeBorder,
+    },
+    sphereDone: {
+        borderColor: commonStyle.firstInitScreen.bottomButtons.done,
+    },
+    sphereInactive: {
+        borderColor: commonStyle.firstInitScreen.statusSphere.inactiveBorder,
+    },
+    sphereError: {
+        borderColor: commonStyle.firstInitScreen.bottomButtons.error,
+    },
+    statusSphereLabel: {
+        fontFamily: commonStyle.fontFamilyExtraBold,
+        fontSize: commonStyle.fontSize.pageTitle,
+        textAlign: "center",
+    },
+    labelActive: {
+        color: commonStyle.firstInitScreen.statusSphere.activeBorder,
+    },
+    labelDone: {
+        color: commonStyle.firstInitScreen.bottomButtons.done,
+    },
+    labelError: {
+        color: commonStyle.firstInitScreen.bottomButtons.error,
+    },
+    labelInactive: {
+        color: commonStyle.firstInitScreen.statusSphere.inactiveBorder,
+    },
+    divider: {
+        width: "30%",
+        height: commonStyle.firstInitScreen.divider.height,
+        backgroundColor:
+            commonStyle.firstInitScreen.statusSphere.inactiveBorder,
+        alignSelf: "flex-end",
+        marginBottom:
+            commonStyle.firstInitScreen.statusSphere.size / 2 -
+            commonStyle.firstInitScreen.divider.height / 2,
+        marginHorizontal: 5,
+    },
+    dividerDone : {
+        backgroundColor: commonStyle.firstInitScreen.bottomButtons.done,
+    },
+    bottomButonsContainer: {
+        position: "absolute",
+        width: "100%",
+        bottom: 0,
+        paddingBottom: 10,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    bottomButtons: {
+        width: commonStyle.firstInitScreen.bottomButtons.size,
+        height: commonStyle.firstInitScreen.bottomButtons.size,
+        borderRadius: commonStyle.firstInitScreen.bottomButtons.size / 2,
+        justifyContent: "center",
+        alignItems: "center",
+        elevation: 1,
+    },
+    bottomButtonsInactive: {
+        backgroundColor: commonStyle.firstInitScreen.bottomButtons.inactive,
+        borderColor: commonStyle.firstInitScreen.bottomButtons.inactiveBorder,
+    },
+    bottomButtonsActive: {
+        backgroundColor: commonStyle.firstInitScreen.statusSphere.activeBorder,
+    },
+    bottomButtonsDone: {
+        backgroundColor: commonStyle.firstInitScreen.bottomButtons.done,
+    },
+    bottomButtonsError: {
+        backgroundColor: commonStyle.firstInitScreen.bottomButtons.error,
+    },
 })
 
 export default FirstInitScreen
