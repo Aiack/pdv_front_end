ae064e8bed90300a66e503f3499867193a86e97d
Jhelison <jhelisong@gmail.com>
2021-05-27 11:34:19 -0300

    Long press on connection error now go to first ini

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -77,6 +77,9 @@ export default (props) => {
                             onPress={() => {
                                 makeConnection()
                             }}
+                            onLongPress={() => {
+                                changeToFirstInit()
+                            }}
                         >
                             <IconFeather
                                 name="rotate-ccw"
